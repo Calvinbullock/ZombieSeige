915d3a6e2e5b481bea2a2939c256f298c9b95827
HyrumB <hyrumbullock@gmail.com>
2024-01-31 15:01:46 -0700

    started gun class

---
+++ scripts/gun.js
@@ -0,0 +1,33 @@
+class Gun {
+
+    // --- Var ---
+
+
+    #ammo // int
+    #name // str
+    #damage // int
+    #bullet_duration // double
+    #fire_sfx // filepath
+    #reload_sfx // filepath
+    #img // filepath
+
+    // constructor(ammo, name, damage, bullet_duration, fire_sfx, reload_sfx, img) {
+    //     this.ammo =ammo
+    //     this.name =name
+    //     this.damage =damage
+    //     this.bullet_duration =bullet_duration
+    //     this.fire_sfx =fire_sfx
+    //     this.reload_sfx =reload_sfx
+    //     this.img =img
+        
+    
+    // }
+
+
+    // --- methods ---
+
+    // check_ammo
+    // remove_ammo
+    //refill_ammo
+    // shoot
+}
