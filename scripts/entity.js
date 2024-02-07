256fd1857a66c263310434cd1ea8b0fcc42483ea
HyrumB <hyrumbullock@gmail.com>
2024-02-07 14:44:36 -0700

    fixed incorrect variable assignments added method to reset health

--- scripts/entity.js
+++ scripts/entity.js
@@ -4,18 +4,25 @@ class Entity {
     #pos_y;
     #speed;
     #health;
+    #max_health;
+    #img;
     #direction;
     
-    constructor(sprite_in, direction_in, health_in, speed_in, pos_y_in) {
+    constructor(sprite_in, direction_in, health_in, speed_in, img_in pos_y_in) {
         this.#pos_x = sprite_in;
         this.#pos_y = direction_in;
-        this.#speed = health_in;
-        this.#health = speed_in;
+        this.#speed = speed_in;
+        this.#max_health = health_in;
+        this.img = img_in;
         this.#direction = pos_y_in;
     }
 
     moveBy() { }
 
     update_health() { }
+
+    reset_health() {
+        health = this.#max_health;
+    }
 }
 
--- scripts/gun.js
+++ scripts/gun.js
@@ -3,27 +3,27 @@ class Gun {
     // --- Var ---
 
 
-    #current_ammo // int
-    #max_ammo // int
-    #name // str
-    #damage // int
-    #bullet_duration // double
-    #bullet_count // int - the amount of bullets spawned when fire button clicked
-    #fire_sfx // filepath
-    #reload_sfx // filepath
-    #img // filepath
+    #current_ammo; // int
+    #max_ammo; // int
+    #name; // str
+    #damage; // int
+    #bullet_duration; // double
+    #bullet_count; // int - the amount of bullets spawned when fire button clicked
+    #fire_sfx; // filepath
+    #reload_sfx; // filepath
+    #img; // filepath
 
-
-    constructor(current_ammo, max_ammo, name, damage, bullet_duration, bullet_count, fire_sfx, reload_sfx, img) {
-        this.#current_ammo =current_ammo 
-        this.#max_ammo =max_ammo
-        this.#name =name
-        this.#damage =damage
-        this.#bullet_duration =bullet_duration
-        this.#bullet_count =bullet_count
-        this.#fire_sfx =fire_sfx
-        this.#reload_sfx =reload_sfx
-        this.#img =img
+    
+    constructor(current_ammo_in, max_ammo_in, name_in, damage_in, bullet_duration_in, bullet_count_in, fire_sfx_in, reload_sfx_in, img_in_in) {
+        this.#current_ammo = current_ammo_in; 
+        this.#max_ammo = max_ammo_in;
+        this.#name = name_in;
+        this.#damage = damage_in;
+        this.#bullet_duration = bullet_duration_in;
+        this.#bullet_count = bullet_count_in;
+        this.#fire_sfx = fire_sfx_in;
+        this.#reload_sfx = reload_sfx_in;
+        this.#img = img_in;
         
     }
 
@@ -32,43 +32,43 @@ class Gun {
 
     // --getters--
     get_name() {
-        return this.#name
+        return this.#name;
     }
 
     get_damage() {
-        return this.#damage
+        return this.#damage;
     }
 
     get_bullet_duration() {
-        return this.#bullet_duration
+        return this.#bullet_duration;
     }
 
     get_bullet_count() {
-        return this.#bullet_count
+        return this.#bullet_count;
     }
     get_fire_sfx() {
-        return this.#fire_sfx
+        return this.#fire_sfx;
     }
 
     get_reload_sfx() {
-        return this.#reload_sfx
+        return this.#reload_sfx;
     }
 
     get_img() {
-        return this.#img
+        return this.#img;
     }
 
     get_ammo() {
-        return this.#current_ammo
+        return this.#current_ammo;
     }
 
     // --setters--
     remove_ammo(casing) {
-        casing -= this.#current_ammo
+        casing -= this.#current_ammo;
     }
 
     refill_ammo() {
-        this.#current_ammo = this.#max_ammo
+        this.#current_ammo = this.#max_ammo;
     }
 
     shoot() {
