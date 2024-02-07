5404f3308b27c6a8203c420a4c67d26339c0fde8
Calvinbullock <calvinnbullock@gmail.com>
2024-02-07 15:02:38 -0700

    entity.js update

--- scripts/entity.js
+++ scripts/entity.js
@@ -5,17 +5,18 @@ class Entity {
     #speed;
     #health;
     #max_health;
-    #img;
+    #sprite;
     #direction;
     
     // added a comma since there was an error ;)
-    constructor(sprite_in, direction_in, health_in, speed_in, img_in, pos_y_in) {
-        this.#pos_x = sprite_in;
-        this.#pos_y = direction_in;
+    constructor(sprite_in, direction_in, health_in, max_health_in, speed_in, img_in, pos_x_in, pos_y_in) {
+        this.#pos_x = pos_x_in;
+        this.#pos_y = pos_y_in;
         this.#speed = speed_in;
-        this.#max_health = health_in;
-        this.img = img_in;
-        this.#direction = pos_y_in;
+        this.health = health_in;
+        this.#max_health = max_health_in;
+        this.sprite = sprite_in;
+        this.#direction = direction_in;
     }
 
     moveBy() { }
