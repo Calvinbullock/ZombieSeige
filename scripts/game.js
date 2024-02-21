b7b3fc46cbc2ba0c351ea984cc1f5389168bc7c1
HyrumB <hyrumbullock@gmail.com>
2024-02-21 16:05:41 -0700

    reformatted and added a test zombie

--- scripts/game.js
+++ scripts/game.js
@@ -1,42 +1,50 @@
-import { Player } from './player.js';
-import { Map } from './map.js';
-import { Camera } from './camera.js';
+import { Player } from "./player.js";
+import { Zombie } from "./zombie.js";
+import { Map } from "./map.js";
+import { Camera } from "./camera.js";
 export class Game {
-
-  player; 
+  player;
   #zombies;
   #round;
   #map;
   #direction;
   canvas;
   #camera;
-  constructor(){
-    this.player = new Player("gun1", "gun2", "./assets/player_male.png", 'direction_in', 100, 100, 100);
-    this.canvas = document.querySelector("#myCanvas").getContext('2d');
+  constructor() {
+    this.player = new Player("gun1", "gun2", "./assets/player_male.png", "direction_in", 100, 100, 100);
+    // makes test zombie
+    this.testZombie = new Zombie(25, "./assets/zombie_fem.png", "direction_in", 100, 100, 100);
+    this.canvas = document.querySelector("#myCanvas").getContext("2d");
     this.#map = new Map();
     let x = this.#map.getWidth();
     let y = this.#map.getHeight();
-    this.#camera = new Camera(x,y);
+    this.#camera = new Camera(x, y);
   }
 
-  gameLoop() 
-  {
+  gameLoop() {
     this.#moveEntities();
     this.#drawScreen();
   }
 
-  #checkColisions() { }
+  #checkColisions() {}
 
-  #moveEntities() 
-  { 
+  #moveEntities() {
     this.player.move();
   }
 
-  #spawnZombies() { }
+  #spawnZombies() {
+    let zomX = this.testZombie.getX();
+    let zomY = this.testZombie.getY();
 
-  #drawScreen() { 
-    let playerX = (this.player.getX())
-    let playerY = (this.player.getY())
+    console.log("zombie x position " + zomX);
+    console.log("zombie y position " + zomY);
+
+    this.testZombie.Draw(this.canvas, zomX, zomY);
+  }
+
+  #drawScreen() {
+    let playerX = this.player.getX();
+    let playerY = this.player.getY();
 
     // console.log("player x position " + playerX);
     // console.log("player y position " + playerY);
@@ -48,12 +56,10 @@ export class Game {
     // console.log("player x screen position "+ x);
     // console.log("player y screen position "+ y);
 
-
     // clear canvas
     this.canvas.clearRect(0, 0, 600, 400);
 
     // draw map
-
 
     let mapX = this.#camera.getMapX(playerX);
     let mapY = this.#camera.getMapY(playerY);
@@ -66,11 +72,10 @@ export class Game {
     // console.log("Map X index " + mapXIndex);
     // console.log("Map Y index " + mapYIndex);
 
-    this.#map.draw(mapX,mapY,mapXIndex,mapYIndex);
-
+    this.#map.draw(mapX, mapY, mapXIndex, mapYIndex);
 
     //draw player
-    this.player.Draw(this.canvas,x,y);
+    this.player.Draw(this.canvas, x, y);
   }
 
   #damage() {}
