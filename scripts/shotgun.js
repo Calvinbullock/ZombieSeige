7cc03a92618cc864ac94d976755a7e45c187a49c
HyrumB <hyrumbullock@gmail.com>
2024-03-05 14:57:46 -0700

    gave the shotgun an even 5 reloads

--- scripts/shotgun.js
+++ scripts/shotgun.js
@@ -4,7 +4,7 @@ export class Shotgun extends Gun {
     #duration;
     
     constructor() {
-      super(50, "Shotgun", 50, 10, 3, "temp", "temp", "assets/shotgun.png",.32,12)
+      super(60, "Shotgun", 60, 10, 3, "temp", "temp", "assets/shotgun.png",.32,12)
 
     }
   }
