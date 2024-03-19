0ab10eefa7400f7f3fb423e01d12206693758127
Tyler M LeFevre <46289255+TLeFevre2003@users.noreply.github.com>
2024-03-19 11:36:33 -0600

    implement fences

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,6 @@
 import { Grass } from "./grass.js";
 import { Edge } from "./edge.js";
+import { Wall } from "./wall.js";
 
 export class Map {
   #path = "./assets/map.txt";
@@ -10,6 +11,14 @@ export class Map {
   #pixelHeight;
   #grass = new Grass();
   #edge = new Edge();
+  #bottom_fence = new Wall("./assets/fence_bottom.png");
+  #bottom_left_fence = new Wall("./assets/fence_corner_bottom_left.png");
+  #bottom_right_fence = new Wall("./assets/fence_corner_bottom_right.png");
+  #top_left_fence = new Wall("./assets/fence_corner_top_left.png");
+  #top_right_fence = new Wall("./assets/fence_corner_top_right.png");
+  #left_fence = new Wall("./assets/fence_left.png");
+  #right_fence = new Wall("./assets/fence_right.png");
+  #top_fence = new Wall("./assets/fence_top.png");
 
   constructor() {
   }
@@ -20,30 +29,62 @@ export class Map {
         .then((response) => response.text())
         .then((data) => {
           const rows = data.trim().split("\n");
-          this.#mapArray = rows.map((row) => row.split(",").map(Number));
+          this.#mapArray = rows.map((row) => row.split(","));
+  
           this.#height = this.#mapArray.length;
           this.#width = this.#height > 0 ? this.#mapArray[0].length : 0;
           console.log("Map Array:", this.#mapArray);
           console.log("Width:", this.#width);
           console.log("Height:", this.#height);
-
+  
           this.#pixelWidth = this.#width * 32;
           this.#pixelHeight = this.#height * 32;
-
+  
           console.log(this.#pixelWidth);
           console.log(this.#pixelHeight);
-
+  
           // Iterate through the map array and replace values with instances of Grass or Edge
           for (let y = 0; y < this.#height; y++) {
             for (let x = 0; x < this.#width; x++) {
-              if (this.#mapArray[y][x] === 1) {
-                this.#mapArray[y][x] = this.#grass;
-              } else if (this.#mapArray[y][x] === 2) {
-                this.#mapArray[y][x] = this.#edge;
+              const cellValue = this.#mapArray[y][x];
+              switch (cellValue) {
+                case '1':
+                  this.#mapArray[y][x] = this.#grass;
+                  break;
+                case '2':
+                  this.#mapArray[y][x] = this.#edge;
+                  break;
+                case '3':
+                  this.#mapArray[y][x] = this.#bottom_fence;
+                  break;
+                case '4':
+                  this.#mapArray[y][x] = this.#bottom_left_fence;
+                  break;
+                case '5':
+                  this.#mapArray[y][x] = this.#bottom_right_fence;
+                  break;
+                case '6':
+                  this.#mapArray[y][x] = this.#top_left_fence;
+                  break;
+                case '7':
+                  this.#mapArray[y][x] = this.#top_right_fence;
+                  break;
+                case '8':
+                  this.#mapArray[y][x] = this.#left_fence;
+                  break;
+                case '9':
+                  this.#mapArray[y][x] = this.#right_fence;
+                  break;
+                case 'a':
+                  this.#mapArray[y][x] = this.#top_fence;
+                  break;
+                default:
+                  // Handle other cases or unknown values
+                  break;
               }
             }
           }
-
+  
           console.log("Map Array After Replacement:", this.#mapArray); // Check the map array after replacements
           resolve(); // Resolve the promise once the map is loaded
         })
@@ -53,6 +94,7 @@ export class Map {
         });
     });
   }
+  
 
   getMapArray() {
     return this.#mapArray;
@@ -101,14 +143,12 @@ export class Map {
 
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 6; j++) {
-        // Perform some action or call some function here
-        // For demonstration, let's log the coordinates
 
         let x_index = xindex + i;
         let y_index = yindex + j;
 
-            let x_pos = x + i * 32;
-            let y_pos = y + j * 32;
+        let x_pos = x + i * 32;
+        let y_pos = y + j * 32;
 
         if (x_index < this.#width && y_index < this.#height) {
           let tile = this.#mapArray[y_index][x_index];
---
+++ scripts/wall.js
@@ -0,0 +1,9 @@
+import { Tile } from "./tile.js";
+
+export class Wall extends Tile {    
+    constructor(path) {
+      super(false,false,path)
+
+    }
+  }
+  
