45865dcd980ccac9bd2d37a9da8cbfaefbd2de87
Tyler M LeFevre <46289255+TLeFevre2003@users.noreply.github.com>
2024-03-18 23:19:59 -0600

    only draws health bar when damaged

--- scripts/zombie.js
+++ scripts/zombie.js
@@ -78,24 +78,30 @@ export class Zombie extends Entity {
     if (screenPositionX <= 256 && screenPositionY <= 128 && screenPositionX >= -16 && screenPositionY >= -16) {
         ctx.drawImage(this.getSpriteLeft(), screenPositionX, screenPositionY);
 
-        // draw the black backround for the health bar
-        ctx.beginPath();
-        ctx.lineWidth = "1";
-        ctx.strokeStyle = "black";
-        ctx.rect(screenPositionX + 2, screenPositionY, 10, 0.5);
-        ctx.stroke();
-
-        // find the blue healthbar width
+        // get zombie health
         let maxHealth = this.getMaxHealth();
         let health = this.getHealth();
-        let healthPercent = health / maxHealth;
-        let healthWidth = 10 * healthPercent;
+        // draw only if zombie is damaged
+        if (health != maxHealth)
+        {
+          // draw the black backround for the health bar
+          ctx.beginPath();
+          ctx.lineWidth = "1";
+          ctx.strokeStyle = "black";
+          ctx.rect(screenPositionX + 2, screenPositionY, 10, 0.5);
+          ctx.stroke();
 
-        // draw blue health bar
-        ctx.beginPath();
-        ctx.strokeStyle = "blue";
-        ctx.rect(screenPositionX + 2, screenPositionY, healthWidth, 0.5);
-        ctx.stroke();
+          // find the blue healthbar width
+          let healthPercent = health / maxHealth;
+          let healthWidth = 10 * healthPercent;
+
+          // draw blue health bar
+          ctx.beginPath();
+          ctx.strokeStyle = "blue";
+          ctx.rect(screenPositionX + 2, screenPositionY, healthWidth, 0.5);
+          ctx.stroke();
+        }
+
     }
 }
 
